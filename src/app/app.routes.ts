65a7625f096f32a44da7651eec07897106f3bf9b
Jeff Lin <Jefflin2000work@gmail.com>
2026-01-13 15:46:22 -0800

    chore: update app configuration and dependencies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Dashboard } from './components/dashboard/dashboard';
 import { FormFillingComponent } from './componenets/form-filling/form-filling';
 import { FormsList } from './components/forms-list/forms-list';
 import { FormSelector } from './components/form-selector/form-selector';
+import { FormReview } from './components/form-review/form-review';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: Dashboard },
     { path: 'new-form', component: FormSelector },
     { path: 'form/isp-2519', component: FormFillingComponent },
+    { path: 'review/:formId', component: FormReview },
     { path: 'forms', component: FormsList },
 ];
